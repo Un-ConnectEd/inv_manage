1cb5a77825d4f2b37458e38e57773560dc9816d3
Un-ConnectEd <144482542+Un-ConnectEd@users.noreply.github.com>
2024-05-07 00:54:47 +0530

    Add files via upload

---
+++ soft/trigger.sql
@@ -0,0 +1,56 @@
+
+DELIMITER $$
+CREATE TRIGGER deli
+AFTER INSERT ON ordert
+FOR EACH ROW
+BEGIN
+    INSERT INTO orderdelivery (OrderID) VALUES (NEW.OrderID);
+END$$
+DELIMITER ;
+
+
+DELIMITER $$
+CREATE TRIGGER ProDel
+AFTER UPDATE ON orderdelivery
+FOR EACH ROW
+BEGIN
+    DECLARE proId INT;
+    DECLARE OrdId INT;
+    DECLARE nos INT;
+    DECLARE done INT DEFAULT 0;
+    DECLARE cur CURSOR FOR
+        SELECT ProductID, OrderID
+        FROM orderproducts
+        WHERE OrderID = NEW.OrderID;
+	
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+    IF NEW.DeliveryStatus = 'Delivered' THEN
+        OPEN cur;
+        read_loop: LOOP
+            FETCH cur INTO proId, OrdId;
+            IF done THEN
+                LEAVE read_loop;
+            END IF;
+            SET nos = (SELECT Quantity FROM orderproducts WHERE ProductID = proId AND OrderID = OrdId);
+            UPDATE inventory SET WarehouseStockLevel = WarehouseStockLevel + nos WHERE ProductID = proId;
+        END LOOP;
+        CLOSE cur;
+    END IF;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER ProBill
+AFTER insert ON billproducts
+FOR EACH ROW
+BEGIN
+	UPDATE inventory SET WarehouseStockLevel = (WarehouseStockLevel - NEW.Quantity) WHERE ProductID = NEW.ProductID;
+END$$
+DELIMITER ;
+
+
+
+
+
+
